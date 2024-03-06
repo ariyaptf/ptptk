e97a4c8ecf56abfc92812bed59671ec6af17e020
ariyaptf <ariya.ptf@gmail.com>
2024-03-06 11:42:32 +0000

    Add new files and update existing files

---
+++ ptptk/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "w#db!o07l%v8sg5m8p7-o8x#6@t0xe5$qmw+z5k%jo&9=px2qs"
+
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
